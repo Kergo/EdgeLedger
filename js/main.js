30a246044933d484dfc60480948fdfb28d70b4a2
Kergo <sskamenov@gmail.com>
2020-04-02 10:56:35 +0200

    Contact, Footer, Mobile

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 42.366112, lng: -71.051542 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Scroll to specific values
+// scrollTo is the same
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 100, // could be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+// Scroll to a certain element
+// document.querySelector('.contact').scrollIntoView({ 
+//   behavior: 'smooth' 
+// });
